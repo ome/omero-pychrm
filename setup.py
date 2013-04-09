4dff3ca28a138bccdbc5ac20a2dd8681cf5fb0ad
Simon Li <spli@dundee.ac.uk>
2013-04-09 18:44:18 +0100

    Attempting to get dependencies with pip to work

--- setup.py
+++ setup.py
@@ -17,10 +17,9 @@ setup(
     description='Scripts for using PyCHRM in OMERO',
     long_description=open('README.md').read(),
     install_requires=[
-        #'pychrm>=0.1.0',
-        'pychrm',
+        'pychrm>=0.1.0',
         ],
-    dependency_links=['svn+http://wnd-charm.googlecode.com/svn/pychrm/trunk/@675#egg=pychrm'],
+    dependency_links=['svn+http://wnd-charm.googlecode.com/svn/pychrm/trunk/@675#egg=pychrm-0.1.0'],
 )
 
 
